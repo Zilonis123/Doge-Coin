bbf1f346ada7b3dac03a22d22910a5e0a178ca8a
Zilonis123 <85059817+Zilonis123@users.noreply.github.com>
2021-08-12 14:10:19 +0300

    cooldown

--- events/message.js
+++ events/message.js
@@ -41,7 +41,7 @@ client.on('messageCreate', async(message) => {
 		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 		if (now < expirationTime) {
 			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`Easier there, **${cmdName}** is on cooldown for ${timeLeft.toFixed(1)}s!`);
+			return message.reply(`Easier there, you will be able to use **${cmdName}** \`${moment().seconds(timeLeft).fromNow()}\``);
 		}
 	}
 	timestamps.set(message.author.id, now);

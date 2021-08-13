bec4e61eb34b15e45979e643a63d3970e2637349
Zilonis123 <85059817+Zilonis123@users.noreply.github.com>
2021-08-13 12:20:38 +0300

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -1,18 +1,19 @@
 const { MessageEmbed } = require('discord.js');
+const moment = require('moment')
 
 module.exports = {
     name: 'stats',
     cooldown: 5,
     description: 'See the bots stats',
     async execute(message, args, client) {
-        let m;
+        let m = 0;
         client.guilds.cache.forEach(g => m += g.memberCount);
         const embed = new MessageEmbed()
             .setColor('YELLOW')
             .setAuthor('Stats')
             .addField('Loyal members', `${m}`, true)
             .addField('Loyal servers', `${client.guilds.cache.size}`, true)
-            .addField('My legal age', `${client.user.createdAt}`, true);
+            .addField('I was created at', `${moment(client.user.createdAt).fromNow()}`, true);
         message.reply({ embeds: [embed] });
     }
 }

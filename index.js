56d7076e1784a42254fed8a325ce0d8608a80344
Zilonis123 <85059817+Zilonis123@users.noreply.github.com>
2021-08-16 16:20:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,34 @@ const client = new Client({
 	partials: ['MESSAGE', 'REACTION', 'CHANNEL'],
 });
 // Checking if dbl is ready
-const dbl = require('dblapi.js');
-const DBL = new dbl(process.env.DBLTOKEN, { webhookPort: 5000, webhookAuth: process.env.DBLAUTH });
-DBL.webhook.on('ready', () => {
-	console.log('DBL is ready!');
-});
+const express = require('express')
+const Topgg = require('@top-gg/sdk')
+const fetch = require('node-fetch')
+const app = express()
+
+const webhook = new Topgg.Webhook('topggauth123') //Secure Password (Change it for god's Sake)
+
+app.post('/vote', webhook.listener(vote => { //ending url
+    console.log("User with id - " + vote.user + " Voted!")
+    let value = JSON.stringify({
+        embeds: [
+            {
+                title: "Another Vote!!",
+                description: `<@${vote.user}> (${vote.user}) Just Voted For \`The Bot\`!!`,
+                color: "8388736" //Hex -> Decimal
+            }
+        ]
+    })
+    fetch("https://discord.com/api/webhooks/876788316723372042/gnY7wD3oyfNwvMplcBmtkSqFA9sh2IRph-CYiWZIIp9Ws3661RIA79zw8e9Arth4uBos", { //Your webhook here
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: value
+    }).catch(e => console.log('Error occured while posting webhook : ' + e))
+}))
+app.listen(3000) //Port
+console.log("Your app is ready to log votes :D")
 
 // Collections
 client.commands = new Collection();
@@ -50,37 +73,5 @@ mongoose.connect(process.env.MONGO, {
 }).catch((err) =>{
     console.log(err);
 });
-// Vote tracking
-DBL.webhook.on('vote', async(vote) => {
-	console.log(`${vote.user} has just voted for ${client.user.tag} in top.gg`);
-	const webclient = new WebhookClient({ id: '876788316723372042', token: 'gnY7wD3oyfNwvMplcBmtkSqFA9sh2IRph-CYiWZIIp9Ws3661RIA79zw8e9Arth4uBos' });
-	webclient.send({
-		username: 'Doge Coin',
-		avatarURL: 'https://cdn.discordapp.com/avatars/873964681721679902/99d7f0d7011fc999b289527cef71b3a1.webp?size=1024',
-		content: `${vote.user} has just voted for ${client.user.tag} in top.gg!`
-	});
-	const inventory = require('./models/inventory');
-	inventory.findOne({ User: vote.user }, async(err, data) => {
-		if (data) {
-			const hasItem = Object.keys(data.Inventory).includes('voter box');
-			if (hasItem) {
-				data.Inventory['voter box']++;
-			}
-			else {
-				data.Inventory['voter box'] = 1;
-			}
-			await inventory.findOneAndUpdate({ User: vote.user }, data);
-		}
-		else {
-			new inventory({
-				User: vote.user,
-				Inventory: {
-					'voter box': 1,
-				}
-			}).save();
-		}
-	})
-});
-// LOG IN
 
 client.login(process.env.TOKEN);

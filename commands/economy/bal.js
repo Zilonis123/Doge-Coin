8115e8619a9ca59e4a96b62d84f337098838ae8e
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-09-01 22:13:00 +0300

    Good

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -15,17 +15,19 @@ module.exports = {
             if(!schem) {
                 const sch = await create(message.author, 0, 0);
                 const embed = new MessageEmbed()
-                    .setAuthor(`${user.tag}'s balance`)
+                    .setAuthor(`${user}'s balance`)
                     .setColor('YELLOW')
                     .addField('Wallet', `\`${sch.Wallet.toLocaleString()}\`💵`, true)
-                    .addField('Bank', `\`${sch.Bank.toLocaleString()} || ${sch.BankMax.toLocaleString()}\`💳`, true);
+                    .addField('Bank', `\`${sch.Bank.toLocaleString()} || ${sch.BankMax.toLocaleString()}\`💳`, true)
+                    .addField('Total', `\`${(sch.Bank + sch.Wallet).toLocaleString()}\`🤑`, true);
                 return message.reply({ embeds: [embed] });
             }
             const embed = new MessageEmbed()
-                .setAuthor(`${user.tag}'s balance`)
+                .setAuthor(`${user}'s balance`)
                 .setColor('YELLOW')
                 .addField('Wallet', `\`${schem.Wallet.toLocaleString()}\`💵`, true)
-                .addField('Bank', `\`${schem.Bank.toLocaleString()} || ${schem.BankMax.toLocaleString()}\`💳`, true);
+                .addField('Bank', `\`${schem.Bank.toLocaleString()} || ${schem.BankMax.toLocaleString()}\`💳`, true)
+                .addField('Total', `\`${(schem.Bank + schem.Wallet).toLocaleString()}\`🤑`, true);
             message.reply({ embeds: [embed] })
         } catch(err) {
             console.log(err)

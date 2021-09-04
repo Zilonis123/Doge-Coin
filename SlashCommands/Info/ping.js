b145e6570342a06f12e168ba7cb9e11e76e60e25
Zilonis123 <85059817+Zilonis123@users.noreply.github.com>
2021-09-04 21:33:32 +0300

    commit arson

--- SlashCommands/Info/ping.js
+++ SlashCommands/Info/ping.js
@@ -1,15 +1,17 @@
 const { MessageEmbed } = require("discord.js");
+const moment = require('moment');
 
 module.exports = {
     name: "ping",
     description: "returns the websocket ping",
     type: 'CHAT_INPUT',
     async execute(client, interaction, args) {
+        const uptime = client.uptime / 1000
         const embed = new MessageEmbed()
             .setColor('YELLOW')
             .addFields(
                 { name: 'Ping', value: `${client.ws.ping}ms!`, inline: true },
-                { name: 'Uptime', value: `${client.uptime}!`, inline: true }
+                { name: 'Uptime', value: `${moment().seconds(uptime).fromNow()}!`, inline: true }
             )
         interaction.followUp({ content: 'Pong!', embeds: [embed] });
     }

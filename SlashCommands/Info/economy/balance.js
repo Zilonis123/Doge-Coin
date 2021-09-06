814a69f5d9489521ff99ac5717a9d402ee839e5b
Zilonis123 <85059817+Zilonis123@users.noreply.github.com>
2021-09-06 14:54:32 +0300

    Create balance.js

---
+++ SlashCommands/Info/economy/balance.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const ms = require('ms');
+const schema = require('../../models/wallet');
+const create = require('../../create wallet');
+
+module.exports = {
+    name: "balance",
+    description: "returns a certain someones balance",
+    options: [
+        {
+            name: 'someone',
+            description: 'a someone whos balance you want to check!',
+            type: 'USER',
+            required: false,
+        },
+    ],
+    type: 'CHAT_INPUT',
+    async execute(client, interaction, args) {
+        const [ someone ] = args
+        let user = interaction.guild.members.cache.get(someone) || interaction.author;
+        if (!user) user = message.author;
+        try {
+            const schem = await schema.findOne({ User: user.id });
+            if(!schem) {
+                const sch = await create(message.author, 0, 0);
+                const embed = new MessageEmbed()
+                    .setAuthor(`${user.tag}'s balance`)
+                    .setColor('YELLOW')
+                    .addField('Wallet', `\`${sch.Wallet.toLocaleString()}\`💵`, true)
+                    .addField('Bank', `\`${sch.Bank.toLocaleString()} || ${sch.BankMax.toLocaleString()}\`💳`, true)
+                    .addField('Total', `\`${(sch.Bank + sch.Wallet).toLocaleString()}\`🤑`, true);
+                return interaction.editReply({ embeds: [embed] });
+            }
+            const embed = new MessageEmbed()
+                .setAuthor(`${user.tag}'s balance`)
+                .setColor('YELLOW')
+                .addField('Wallet', `\`${schem.Wallet.toLocaleString()}\`💵`, true)
+                .addField('Bank', `\`${schem.Bank.toLocaleString()} || ${schem.BankMax.toLocaleString()}\`💳`, true)
+                .addField('Total', `\`${(schem.Bank + schem.Wallet).toLocaleString()}\`🤑`, true);
+            interaction.editReply({ embeds: [embed] })
+        } catch(err) {
+            console.log(err)
+        }
+    }
+};

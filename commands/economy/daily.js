3673b58b8bea8c99d5b9fc453fc4ebc0334024ec
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-09-18 12:08:21 +0300

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -2,27 +2,21 @@ const create = require('../../wallet create');
 const schema = require('../../models/wallet');
 const inventory = require('../../models/inventory');
 const { MessageEmbed } = require('discord.js');
-const creates = require('../../daily create');
-const schemas = require('../../models/daily');
-const moment = require('moment');
+const ms = require('ms');
 
 module.exports = {
     name: 'daily',
     aliases: ['d'],
     description: 'Get your daily money',
+    cooldown: 57600,
     async execute(message, args, client) {
-        const isAllowed = await schemas.findOne({ User: message.author.id });
-        const number = moment("24:00:00", "hh:mm:ss").diff(moment(), 'seconds');
-        if (isAllowed) return message.reply(`You can recieve your daily again \`${moment().seconds(number).fromNow()}\``);
-        await creates(message.author);
-        client.daily.set(message.author.id, 'start')
         const coin = client.guilds.cache.get('873965279665860628').emojis.cache.get('874290622201221211');
         const sch = await schema.findOne({ User: message.author.id });
 
-        let text = `You got \`{items}\`<a:${coin.name}:${coin.id}>\n\nCome back in ${moment().seconds(number).fromNow()} here is your daily`
+        let text = `You got \`{items}\`\n\nCome back in ${ms(576000000, { long: true })} here is your daily`
         const random = Math.floor(Math.random() * 10);
         if (random === 1) {
-            text = text.replace('{items}', '25,000\n1 - 📦loot box');
+            text = text.replace('{items}', '25,000<a:${coin.name}:${coin.id}>\n1 - 📦loot box');
             inventory.findOne({ User: message.author.id }, async(err, data) => {
                 if (data) {
                     const hasItem = Object.keys(data.Inventory).includes('loot');
@@ -45,6 +39,8 @@ module.exports = {
                 }
             });
         }
+        text = text.replace('{items}', `25,000 <a:${coin.name}:${coin.id}>`)
+
         if (!sch) {
             await create(message.author, 25000, 0);
             return message.channel.send({ embeds: [new MessageEmbed().setColor('YELLOW').setDescription(`You got \`25,000\`<a:${coin.name}:${coin.id}>\n\nCome back in ${moment().seconds(number).fromNow()}`).setAuthor(`${message.author.tag} here is your daily`)] });

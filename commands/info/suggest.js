58e2c41bc801111ebdafaf71a2810724ed4ec222
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-09-24 15:38:04 +0300

    Update suggest.js

--- commands/info/suggest.js
+++ commands/info/suggest.js
@@ -7,6 +7,12 @@ module.exports = {
         if (!args[0]) return message.reply(`${message.author.username} please add a suggestion..`);
         if (args.length < 3) return message.reply('You are a fool you cant suggest something that small.. **whispers** do you know what else is small?');
         const webclient = new WebHookClient({ id: 890933789700788234, token: 'Sav6QKUBgfwZmfE2X38oQ3QvI6DuBMy_x-3-lm-s1CrLQyFQWBVjmWd1RVMe2y-JOzrr' });
-        return;
+        await webclient.send({
+            name: message.author.tag,
+            avatar: message.author.displayAvatarURL(),
+            content: args.join(' '),
+        });
+
+        message.reply('Succsesfully send your suggestion! To the support server');
     }
 }

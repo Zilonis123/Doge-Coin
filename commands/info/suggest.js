b85df35d1d836a5b5f1aa969016a35622b2ba9e3
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-09-24 15:52:18 +0300

    Update suggest.js

--- commands/info/suggest.js
+++ commands/info/suggest.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, WebHookClient } = require('discord.js');
+const { MessageEmbed, WebhookClient } = require('discord.js');
 module.exports = {
     name: 'suggest',
     description: 'suggest a command.. this will send a message in the support server!',
@@ -6,10 +6,10 @@ module.exports = {
     async execute(message, args, client) {
         if (!args[0]) return message.reply(`${message.author.username} please add a suggestion..`);
         if (args.length < 3) return message.reply('You are a fool you cant suggest something that small.. **whispers** do you know what else is small?');
-        const webclient = new WebHookClient({ id: '890933789700788234', token: 'Sav6QKUBgfwZmfE2X38oQ3QvI6DuBMy_x-3-lm-s1CrLQyFQWBVjmWd1RVMe2y-JOzrr' });
+        const webclient = new WebhookClient({ id: '890933789700788234', token: 'Sav6QKUBgfwZmfE2X38oQ3QvI6DuBMy_x-3-lm-s1CrLQyFQWBVjmWd1RVMe2y-JOzrr' });
         await webclient.send({
-            name: message.author.tag,
-            avatar: message.author.displayAvatarURL(),
+            username: message.author.tag,
+            avatarURl: message.author.displayAvatarURL(),
             content: args.join(' '),
         });
 

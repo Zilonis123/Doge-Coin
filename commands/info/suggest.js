27e8cc51b0e1dc8969c0a07ca101a3b9ceaf8873
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-09-24 16:17:33 +0300

    Update suggest.js

--- commands/info/suggest.js
+++ commands/info/suggest.js
@@ -13,6 +13,9 @@ module.exports = {
             content: args.join(' '),
         });
 
+        await msg.react('👍');
+        await msg.react('👎');
+
         message.reply('Succsesfully send your suggestion! To the support server');
     }
 }

48fdf6f960bf065fb63c766ee88a580e506c56ab
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-01 21:19:30 +0300

    Update suggest.js

--- commands/info/suggest.js
+++ commands/info/suggest.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'suggest a command.. this will send a message in the support server!',
     aliases: ['suggestion'],
     async execute(message, args, client) {
-        if (!args[0] || message.mentions.first()) return message.reply(`${message.author.username} please suggest a valid suggestion.`);
+        if (!args[0] || message.mentions.users.first()) return message.reply(`${message.author.username} please suggest a valid suggestion.`);
         if (args.length < 3) return message.reply('You are a fool you cant suggest something that small.. **whispers** do you know what else is small?');
         const webclient = new WebhookClient({ id: '890933789700788234', token: 'Sav6QKUBgfwZmfE2X38oQ3QvI6DuBMy_x-3-lm-s1CrLQyFQWBVjmWd1RVMe2y-JOzrr' });
         const msg = await webclient.send({

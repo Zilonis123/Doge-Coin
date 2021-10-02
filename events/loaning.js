3f719b30eb147cb6107f23a3448d7c887bdd84ed
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-03 00:45:00 +0300

    Update loaning.js

--- events/loaning.js
+++ events/loaning.js
@@ -5,13 +5,12 @@ client.on('messageCreate', message => {
     if (message.author.id === '873964681721679902' && message.channel.id === '893852129549234206') {
         const items = message.content.split(' ');
         client.loans.set(items[0], items[1]);
-        console.log(items)
         setTimeout(async function() {
             message.delete();
             const ply = await Player.findOne({ User: items[0] });
-            ply.Wallet -= items[1];
+            ply.Wallet -= parseInt(items[1]);
             ply.save();
             client.loans.set(items[0], null);
-        }, items[4])
+        }, parseInt(items[4]));
     }
 })

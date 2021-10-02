b9c1c8e9dfa9c603b9b87cc3e6eec05440b3792a
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-02 21:09:34 +0300

    Update loaning.js

--- events/loaning.js
+++ events/loaning.js
@@ -3,6 +3,8 @@ const client = require('../index');
 client.on('message', message => {
     if (message.author.id === '873964681721679902' && message.channel.id === '893852129549234206') {
         const items = message.content.slice(' ');
-        
+        setTimeout(function() {
+            // Do smth
+        }, items[-1])
     }
 })

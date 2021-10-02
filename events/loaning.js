7edd4196cb8eb50459e4c98274986a1e846245af
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-02 23:33:15 +0300

    Update loaning.js

--- events/loaning.js
+++ events/loaning.js
@@ -5,7 +5,7 @@ client.on('message', message => {
     if (message.author.id === '873964681721679902' && message.channel.id === '893852129549234206') {
         const items = message.content.slice(' ');
         client.loans.set(items[0], items[1]);
-        setTimeout(function() {
+        setTimeout(async function() {
             message.delete();
             const ply = await Player.findOne({ User: items[0] });
             ply.Wallet -= items[1];

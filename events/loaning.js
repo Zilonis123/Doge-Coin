4ab7aaf34199edc434f6cedf68573de503602235
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-02 20:56:01 +0300

    Create loaning.js

---
+++ events/loaning.js
@@ -0,0 +1,8 @@
+const client = require('../index');
+
+client.on('message', message => {
+    if (message.author.id === '873964681721679902' && message.channel.id === '893852129549234206') {
+        const items = message.content.slice(' ');
+        
+    }
+})

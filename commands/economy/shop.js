74bc14a75d6e96c327abfb496d151c7219d157e8
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-11 13:38:49 +0300

    Update shop.js

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -85,7 +85,7 @@ module.exports = {
         });
         
         collector.on('collect', (interaction) => {
-            if (interaction.value == 'htl') {
+            if (interaction.value === 'htl') {
                 initalMessage.delete();
                 pagination({
                     message: message,

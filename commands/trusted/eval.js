e35159d0203a085b258618eeca8c42782d0c5a94
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-10-18 13:36:26 +0300

    Update eval.js

--- commands/trusted/eval.js
+++ commands/trusted/eval.js
@@ -1,1 +1,29 @@
+const config = require('../../config.json');
+const { inspect } = require('util');
+const { MessageEmbed } = require('discord.js');
 
+module.exports = {
+  name: 'eval',
+  description: 'ok how did you get here?'
+  aliases: ['exec', 'execute'],
+  async execute(message, args, client) {
+    if (!config.trusted.include(message.author.id)) return;
+    
+    const code = args.join(' ');
+    if (!code) message.reply('Add some code please!');
+    
+    try {
+      const result = await eval(code);\
+      let output = result;
+      
+      if (typeof result !== 'string') {
+        output = inspect(result);
+      }
+      
+      message.channel.send({ content: `${output}`})
+    } catch (error) {
+      console.log(err)
+      message.channel.send('Evaluated content is too long to display!')
+    }
+  }
+}

4d601b7374495c7b1fbe36f374c96eec7a51fcdc
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-01 13:18:30 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -19,20 +19,3 @@ client.on('guildCreate', async(guild) => {
       content: `${role}`,
   })
 });
-
-client.on('guildDelete', async(guild) => {
-  const sguild = await client.guilds.cache.get('873965279665860628');
-  const role = await sguild.roles.cache.get('899593005172924427');
-  
-  const embed = new MessageEmbed()
-    .setColor('YELLOW')
-    .setFooter('RIP')
-    .setDescription('Doge coin has left a guild.. really sad hopefully noone says anything in general!')
-    .setAuthor('Left a server');
-  webclient.send({
-      username: 'Leave logs',
-      avatarURL: client.user.displayAvatarURL(),
-      embeds: [embed],
-      content: `${role}`,
-  })
-});

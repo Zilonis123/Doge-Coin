0e94652dbd2204b21c2926b9a51e2795ef037403
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-01 13:14:02 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,14 +6,10 @@ const webclient = new WebhookClient({ id: '899596753622429706', token: '-1MVdaYh
 client.on('guildCreate', async(guild) => {
   const sguild = await client.guilds.cache.get('873965279665860628');
   const role = await sguild.roles.cache.get('899593005172924427');
-  const member = await guild.members.cache.get(guild.ownerId);
-  const smember = await sguild.members.cache.get(guild.ownerId);
-  let message = `${member.username} is in the server`;
-  if (!smember) message = `servers owner is not in the server`
   
   const embed = new MessageEmbed()
     .setColor('YELLOW')
-    .setFooter(message)
+    .setFooter("Awesome!!!!")
     .setDescription('Doge coin has joined another happy server!\nLets reach our goal of beeing verified quicker!')
     .setAuthor('Joined a server');
   webclient.send({

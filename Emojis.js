e302b7defc159112242e250223d377bd1b860958
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-06 19:46:01 +0200

    Update Emojis.js

--- Emojis.js
+++ Emojis.js
@@ -6,6 +6,11 @@ module.exports = async(emoji) => {
     if (emoji.includes('coin')) {
         // Require coin emoji from the guild and return the emoji
         const emoji_coin = await guild.emojis.cache.get('874290622201221211');
-        return emoji_coin;
+        return transform(emoji_coin);
     }
+}
+
+const transform = (emoji, type='') => {
+    const em = `<${type}:${emoji.name}:${emoji.id}>`;
+    return em;
 }

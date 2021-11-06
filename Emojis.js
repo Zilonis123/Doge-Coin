f2ac278010e0f858ed7c3bd9ffffbbe91f969dac
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-06 12:06:26 +0200

    Started emoji giving thinggs

---
+++ Emojis.js
@@ -0,0 +1,8 @@
+const client = require('index.js');
+
+modules.exports = async(emoji) => {
+    const guild = await client.guilds.cache.get('873965279665860628');
+    if (emoji.includes('coin') {
+        // Give coin
+    }
+}

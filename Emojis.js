8aef9f93d9b0fe334ab3259d3daa7e2e476c0807
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-06 22:19:44 +0200

    Added laughingpepe emoji

--- Emojis.js
+++ Emojis.js
@@ -9,6 +9,10 @@ module.exports = async(emoji) => {
         // Transform the Object to a usable emoji and return
         return transform(emoji_coin, 'a');
     }
+    if (emoji.includes('pepe') || emoji.includes('lol')) {
+         const emoji_laughingpepe = await guild.emojis.cache.get('874577305928888360');
+         return transform(emoji_laughingpepe, 'a');
+    }
 }
 
 const transform = (emoji, type='') => {

cf17585d79000f2ec592187909bc49601c38c4e0
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-06 22:52:02 +0200

    Fix some errors and use new system

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -10,40 +10,39 @@ module.exports = {
     description: 'Get your daily money',
     cooldown: 86400,
     async execute(message, args, client) {
-        const coin = client.guilds.cache.get('873965279665860628').emojis.cache.get('874290622201221211');
+        const coin = await global.emojis('coin');
         const sch = await schema.findOne({ User: message.author.id });
 
         let text = `You got {items}\n\nCome back in \`1 day\``
         const random = Math.floor(Math.random() * 10);
         if (random === 1) {
-            text = text.replace('{items}', `\`25,000\`<a:${coin.name}:${coin.id}>\n\`1\` - 📦loot box`);
+            text = text.replace('{items}', `\`25,000\`${coin}\n\`1\` - 📦loot box`);
             inventory.findOne({ User: message.author.id }, async(err, data) => {
                 if (data) {
                     const hasItem = Object.keys(data.Inventory).includes('loot');
                     if (!hasItem) {
-                        data.Inventory['loot'] = 1;
+                        data.Inventory['lootbox'] = 1;
                     }
                     else {
-                        data.Inventory['loot'] += 1;
+                        data.Inventory['lootbox'] += 1;
                     }
-                    console.log(data);
                     await inventory.findOneAndUpdate({ User: message.author.id }, data)
                 }
                 else {
                     new inventory({
                         User: message.author.id,
                         Inventory: {
-                            ['loot']: 1
+                            ['lootbox']: 1
                         },
                     }).save();
                 }
             });
         }
-        text = text.replace('{items}', `\`25,000\`<a:${coin.name}:${coin.id}>`)
+        text = text.replace('{items}', `\`25,000\`${coin}`)
 
         if (!sch) {
             await create(message.author, 25000, 0);
-            return message.channel.send({ embeds: [new MessageEmbed().setColor('YELLOW').setDescription(`You got \`25,000\`<a:${coin.name}:${coin.id}>\n\nCome back in ${moment().seconds(number).fromNow()}`).setAuthor(`${message.author.tag} here is your daily`)] });
+            return message.channel.send({ embeds: [new MessageEmbed().setColor('YELLOW').setDescription(`You got \`25,000\`${coin}\n\nCome back in ${moment().seconds(number).fromNow()}`).setAuthor(`${message.author.tag} here is your daily`)] });
         }
         message.channel.send({ embeds: [new MessageEmbed().setColor('YELLOW').setAuthor(`${message.author.tag}`, message.author.displayAvatarURL()).setDescription(`${text}`)] });
         sch.Wallet += 25000,

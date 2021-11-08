32a9d5c976026bded0aa517b9bbbc5cea8cf20a3
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-08 11:54:11 +0200

    update some things

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -8,8 +8,8 @@ module.exports = {
     description: 'Work if you are bored of begging',
     cooldown: 3600,
     async execute(message, args, client) {
-        const lol = client.guilds.cache.get('873965279665860628').emojis.cache.get('874577305928888360');
-        const coin = client.guilds.cache.get('873965279665860628').emojis.cache.get('874290622201221211');
+        const lol = await global.emojis('lol');
+        const coin = await global.emojis('coin');
         const jobs = ['miner', 'footballer'];
         
         const rndom = Math.floor(Math.random() * jobs.length);
@@ -24,14 +24,15 @@ module.exports = {
                 'Why do i only find coal?'
             ],
             footballer: [
-                'MY LEG IS FKING BROKEN',
+                'MY LEG IS FUCKING BROKEN',
                 'We won but at what cost?',
                 'I like hamburgers',
                 '69 against 420 points we are loosing'
             ]
         }
-        const textToImage = require('text-to-image');
-
+        
+        const multiplier = await global.multiplier(message.guild.id, message.author);
+        // Job type
         const type = Math.floor(Math.random() * 3);
         if (type === 2) {
             function scramble(a) {
@@ -96,8 +97,9 @@ module.exports = {
             message.reply({ embeds: [embed], files: [file] })
             const filter = m => m.author.id === message.author.id && m.content.toLowerCase() === words[job][word].toLowerCase();
             const ans = await message.channel.awaitMessages({ filter, max: 1, time: 20000, errors: ['time'] }).catch((err) => {});
-            if (!ans) return message.reply(`You failed! <a:${lol.name}:${lol.id}>\nThe word was **${words[job][word]}**`);
-            const mony = Math.floor(Math.random() * 5000) + 15000;
+            if (!ans) return message.reply(`You failed! ${lol}\nThe word was **${words[job][word]}**`);
+            // calculate money
+            const mony = Math.floor((Math.random() * 5000) * multiplier) + 15000;
             fs.unlink(`${message.author.id}.png`, function (err) {
   
                 if (err) throw err;
@@ -109,7 +111,7 @@ module.exports = {
                 sche.Wallet += mony;
                 sche.save();
             }
-            message.reply(`Great job, you got \`${mony}\`<a:${coin.name}:${coin.id}>`);
+            message.reply(`Great job, you got \`${mony}\`${coin} (**${multiplier}x multiplier**)`);
             return;
         }
 
@@ -137,8 +139,8 @@ module.exports = {
             if (err) throw err;
           }); 
         if (!ans || ans.first().content.toLowerCase() !== text[job][random].toLowerCase()) {
-            const bruh = Math.floor(Math.random() * 1000) + 1;
-            message.reply(`Bad work, you got \`${bruh.toLocaleString()}\`<a:${coin.name}:${coin.id}>`);
+            const bruh = Math.floor((Math.random() * 1000) * multiplier) + 1;
+            message.reply(`Bad work, you got \`${bruh.toLocaleString()}\`${coin} (**${multiplier}x multiplier**)`);
             const schem = await schema.findOne({ User: message.author.id });
             if (!schem) {
                 return create(message.author, bruh, 0);
@@ -147,12 +149,13 @@ module.exports = {
             schem.save();
             return;
         }
-        message.reply(`Good work soldier, you got \`15,000\`<a:${coin.name}:${coin.id}>`);
+        const mony = Math.floor(15000 * multiplier);
+        message.reply(`Good work soldier, you got \`${mony}\`${coin} (**${multiplier}x multiplier**)`);
         const sche = await schema.findOne({ User: message.author.id });
         if (!sche) {
-            return create(message.author, 15000, 0);
+            return create(message.author, mony, 0);
         }
-        sche.Wallet += 15000;
+        sche.Wallet += mony;
         sche.save();
         
     }

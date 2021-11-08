d699ad4232c3d3ac63caa5d39b0015a12cbd8eb3
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-08 09:00:10 +0200

    fix some bugs and return cash

--- Shorteners/multiplier.js
+++ Shorteners/multiplier.js
@@ -1,6 +1,8 @@
 const client = require('../index.js');  
 
-module.exports = const multiplier = async(guild, playerId) => {
-  let mutli = 1;
+module.exports = const multiplier = async(guild, playerId, cash) => {
+  let multi = 1;
   if (client.goldRush.has(guild.id)) multi += 5;
+  
+  return parseInt(cash) * multi;
 }

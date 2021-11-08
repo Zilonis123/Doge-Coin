b811dc06335bdffa59ece5200b840309c5c76d1e
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-08 09:05:24 +0200

    Update Emojis.js

--- Shorteners/Emojis.js
+++ Shorteners/Emojis.js
@@ -3,16 +3,17 @@ const client = require('../index.js');
 module.exports = async(emoji) => {
     // Require the guild
     const guild = await client.guilds.cache.get('873965279665860628');
-    if (emoji.includes('coin')) {
+    if (emoji.toLowerCase() === 'coin') {
         // Require coin emoji from the guild and return the emoji
         const emoji_coin = await guild.emojis.cache.get('874290622201221211');
         // Transform the Object to a usable emoji and return
         return transform(emoji_coin, 'a');
     }
-    if (emoji.includes('pepe') || emoji.includes('lol')) {
+    if (emoji.toLowerCase() === 'lol') {
          const emoji_laughingpepe = await guild.emojis.cache.get('874577305928888360');
          return transform(emoji_laughingpepe, 'a');
     }
+    return null;
 }
 
 const transform = (emoji, type='') => {

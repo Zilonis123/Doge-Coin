85218d6a627ee8b387601ef642a9c0e27a2e2a5f
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-08 10:23:03 +0200

    Update multiplier.js

--- Shorteners/multiplier.js
+++ Shorteners/multiplier.js
@@ -1,10 +1,10 @@
 const client = require('../index.js');  
 
-module.exports = multiplier = async(guild, playerId, cash) => {
+module.exports = multiplier = async(guild, playerId) => {
   // define the multiplier
   let multi = 1;
   // check if the current server has a gold rush
   if (client.goldRush.has(guild.id)) multi += 5;
   
-  return parseInt(cash) * multi, multi;
+  return parseInt(multi);
 }

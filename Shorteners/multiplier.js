e178db0369a0b194a773db0b2bdcc0bb0005b9cf
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-08 09:18:21 +0200

    return the multiplier

--- Shorteners/multiplier.js
+++ Shorteners/multiplier.js
@@ -6,5 +6,5 @@ module.exports = multiplier = async(guild, playerId, cash) => {
   // check if the current server has a gold rush
   if (client.goldRush.has(guild.id)) multi += 5;
   
-  return parseInt(cash) * multi;
+  return parseInt(cash) * multi, multi;
 }

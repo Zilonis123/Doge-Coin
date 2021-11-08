3127fcd84643546aea8a3cdc43bfe09013621639
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-08 13:16:31 +0200

    Update multiplier.js

--- Shorteners/multiplier.js
+++ Shorteners/multiplier.js
@@ -8,14 +8,10 @@ module.exports = multiplier = async(guild, userId) => {
   if (client.goldRush.has(guild.id)) multi += 5;
   // require the inventory
   const inv = await inventory.findOne({ User: userId });
-  console.log(inv.Inventory['police car'])
   if (inv.Inventory['police car'] > 0) {
-    console.log(inv.Inventory['police car'])
     const times = inv.Inventory['police car'];
     multi += times * 0.3;
-    console.log(times * 0.3);
-    console.log(multi)
   }
   
-  return parseInt(multi);
+  return multi;
 }

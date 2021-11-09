8f13762e2f752a91ab62b9e9c3f65dffb853ef5b
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-09 23:46:52 +0200

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -27,7 +27,7 @@ module.exports = {
         let m = await message.reply({ embeds: [
             new Discord.MessageEmbed()
                 .setAuthor("Stats 📊", client.user.displayAvatarURL())
-                .setColor('#A9E9F6')
+                .setColor('YELLOW')
                 .addFields(
                     {
                         name: 'Ping',
@@ -69,7 +69,7 @@ module.exports = {
         m.edit(
             new Discord.MessageEmbed()
                 .setAuthor("Stats 📊", client.user.displayAvatarURL())
-                .setColor('#A9E9F6')
+                .setColor('YELLOW')
                 .addFields(
                     {
                         name: 'Ping',

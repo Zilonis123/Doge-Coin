b35c0f5bed5ec0b80bbcc8d86a6fadc029905f63
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-11-11 18:05:21 +0200

    Update buy.js

--- commands/economy/buy.js
+++ commands/economy/buy.js
@@ -34,6 +34,7 @@ module.exports = {
         inventory.findOne({ User: message.author.id }, async(err, data) => {
         })       
         { 
+            if (!data) break;
             if (((itemName.item == "clock" && data.Inventory[itemName.item] > 0) || (count > 1 && itemName.item === 'clock')) || ((itemName.item == "police car" && data.Inventory[itemName.item] > 99) || (count > 99 && itemName.item === 'police car' || (count + data.Inventory['police car'] >= 100))))
         {
             return message.reply(`You can't own more ${itemName.item}'s than you have!`);

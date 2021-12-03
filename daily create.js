7d5e0ba82bd20c744b8f1ac4484764e88dfaa768
Zilonis123 <kazaks.karlis2009@gmail.com>
2021-12-03 13:58:59 +0200

    Delete a useless file

--- daily create.js
+++
@@ -1,8 +0,0 @@
-const schema = require('./models/daily');
-module.exports = create = async(message, wallet, bank) => {
-    const sch = await schema.create({
-        User: message.id,
-    });
-    sch.save();
-    return sch;
-}
